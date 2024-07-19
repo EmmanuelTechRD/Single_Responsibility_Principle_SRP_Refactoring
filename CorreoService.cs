5a8be77801c15ba0fab92b25c81314f9b85c4144
Emmanuel Soto <emmanueltechrd@gmail.com>
2024-07-19 14:39:11 -0400

    Initial commit

---
+++ CorreoService.cs
@@ -0,0 +1,10 @@
+﻿using System;
+
+public class CorreoService
+{
+    public void EnviarCorreoElectronico(string correoElectronico, string mensaje)
+    {
+        // Lógica para enviar correo electrónico
+        Console.WriteLine($"Enviando correo a {correoElectronico}: {mensaje}");
+    }
+}
